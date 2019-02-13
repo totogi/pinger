0f0118031f654fc4f0c344c9c834281a925ffe13
gregwhitaker <gwhitake@gmail.com>
2019-02-12 18:10:34 -0800

    Doing work

--- pinger-core/src/main/java/pinger/core/DefaultPinger.java
+++ pinger-core/src/main/java/pinger/core/DefaultPinger.java
@@ -3,8 +3,24 @@ package pinger.core;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.net.URI;
+import java.time.Duration;
+
 public class DefaultPinger implements Pinger {
     private static final Logger LOG = LoggerFactory.getLogger(DefaultPinger.class);
 
+    private final URI url;
+    private final String phoneNumber;
+    private final Duration checkInterval;
+    private final Integer errorCount;
 
+    public DefaultPinger(URI url,
+                         String phoneNumber,
+                         Duration checkInterval,
+                         Integer errorCount) {
+        this.url = url;
+        this.phoneNumber = phoneNumber;
+        this.checkInterval = checkInterval;
+        this.errorCount = errorCount;
+    }
 }
--- pinger-core/src/main/java/pinger/core/PingerBuilder.java
+++ pinger-core/src/main/java/pinger/core/PingerBuilder.java
@@ -1,10 +1,41 @@
 package pinger.core;
 
+import com.google.i18n.phonenumbers.NumberParseException;
+import com.google.i18n.phonenumbers.PhoneNumberUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public final class PingerBuilder {
+import java.net.URI;
+import java.time.Duration;
+
+public class PingerBuilder {
     private static final Logger LOG = LoggerFactory.getLogger(PingerBuilder.class);
 
+    private URI url;
+    private String phoneNumber;
+    private Duration duration = Duration.ofMinutes(1);
+    private Integer errorCount = 3;
 
+    public PingerBuilder(URI url, String phoneNumber) throws NumberParseException {
+        this.url = url;
+        this.phoneNumber = normalizePhoneNumber(phoneNumber);
+    }
+
+    public PingerBuilder withInterval(Duration duration) {
+        this.duration = duration;
+        return this;
+    }
+
+    public PingerBuilder withErrorCountBeforeAlert(Integer errorCount) {
+        this.errorCount = errorCount;
+        return this;
+    }
+
+    public Pinger build() {
+        return null;
+    }
+
+    private String normalizePhoneNumber(String phoneNumber) throws NumberParseException {
+        return PhoneNumberUtil.getInstance().parse(phoneNumber, "US").toString();
+    }
 }
