10a5aded94fd2821e7db8f00e6c692fd91c7face
gregwhitaker <gwhitake@gmail.com>
2019-02-12 18:41:45 -0800

    Doing work

--- pinger-core/src/main/java/pinger/core/TwilioPinger.java
+++ pinger-core/src/main/java/pinger/core/TwilioPinger.java
@@ -6,18 +6,18 @@ import org.slf4j.LoggerFactory;
 import java.net.URI;
 import java.time.Duration;
 
-public class DefaultPinger implements Pinger {
-    private static final Logger LOG = LoggerFactory.getLogger(DefaultPinger.class);
+public class TwilioPinger implements Pinger {
+    private static final Logger LOG = LoggerFactory.getLogger(TwilioPinger.class);
 
     private final URI url;
     private final String phoneNumber;
     private final Duration checkInterval;
     private final Integer errorCount;
 
-    public DefaultPinger(URI url,
-                         String phoneNumber,
-                         Duration checkInterval,
-                         Integer errorCount) {
+    public TwilioPinger(URI url,
+                        String phoneNumber,
+                        Duration checkInterval,
+                        Integer errorCount) {
         this.url = url;
         this.phoneNumber = phoneNumber;
         this.checkInterval = checkInterval;
