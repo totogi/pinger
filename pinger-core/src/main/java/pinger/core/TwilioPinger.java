1a14f095c39c92096a23784faadd3575bea4f409
gregwhitaker <gwhitake@gmail.com>
2019-02-12 19:14:51 -0800

    Doing work

--- pinger-core/src/main/java/pinger/core/TwilioPinger.java
+++ pinger-core/src/main/java/pinger/core/TwilioPinger.java
@@ -3,57 +3,85 @@ package pinger.core;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class TwilioPinger implements Runnable {
+import java.time.Duration;
+
+/**
+ *
+ */
+public final class TwilioPinger implements Runnable {
     private static final Logger LOG = LoggerFactory.getLogger(TwilioPinger.class);
 
-    public static TwilioPingerBuilder newBuilder(String account, String authToken) {
-        return new TwilioPingerBuilder(account, authToken);
+    /**
+     *
+     * @param account
+     * @param authToken
+     * @return
+     */
+    public static Builder newBuilder(String account, String authToken) {
+        return new Builder(account, authToken);
     }
 
-    private TwilioPinger() {
+    private final String account;
+    private final String authToken;
+    private final String url;
+    private final String phoneNumber;
+    private final Duration interval;
+    private final Integer threshold;
 
+    private TwilioPinger(final String account,
+                         final String authToken,
+                         final String url,
+                         final String phoneNumber,
+                         final Duration interval,
+                         final Integer threshold) {
+        this.account = account;
+        this.authToken = authToken;
+        this.url = url;
+        this.phoneNumber = phoneNumber;
+        this.interval = interval;
+        this.threshold = threshold;
     }
 
     @Override
     public void run() {
-
+        LOG.info("Monitoring Url: {}", url);
     }
 
-    public static class TwilioPingerBuilder {
-        private String account;
-        private String authToken;
+    public static class Builder {
+        private final String account;
+        private final String authToken;
         private String url;
         private String phoneNumber;
         private Long interval;
         private Integer threshold;
 
-        public TwilioPingerBuilder(String account, String authToken) {
+        public Builder(final String account, final String authToken) {
             this.account = account;
             this.authToken = authToken;
         }
 
-        public TwilioPingerBuilder withUrl(String url) {
+        public Builder withUrl(final String url) {
             this.url = url;
             return this;
         }
 
-        public TwilioPingerBuilder withPhoneNumber(String phoneNumer) {
+        public Builder withPhoneNumber(final String phoneNumer) {
             this.phoneNumber = phoneNumer;
             return this;
         }
 
-        public TwilioPingerBuilder withInterval(Long interval) {
+        public Builder withInterval(final Long interval) {
             this.interval = interval;
             return this;
         }
 
-        public TwilioPingerBuilder withThreshold(Integer threshold) {
+        public Builder withThreshold(final Integer threshold) {
             this.threshold = threshold;
             return this;
         }
 
         public TwilioPinger build() {
-            return new TwilioPinger();
+            return new TwilioPinger(account, authToken, url, phoneNumber, Duration.ofMinutes(interval), threshold);
         }
     }
 }
